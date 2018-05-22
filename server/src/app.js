ad982bd9a02d69e6b28eb989093715899ee1820f
Tess Porter <tcp46@drexel.edu>
2018-05-21 22:48:39 -0400

    setting up frontend and backend

--- server/src/app.js
+++ server/src/app.js
@@ -4,14 +4,14 @@ const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
-app.use(morgan('combine'))
+app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
 // get, post, put, patch, delete
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
   res.send({
-    message: 'hello world!'
+    message: 'Hello your user was registered!'
   })
 })
 
